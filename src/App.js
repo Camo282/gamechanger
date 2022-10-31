558d2c4f678697e333504aa532114d38f24b72b6
Camo282 <camo282@gmail.com>
2022-10-31 19:30:03 -0400

    starting the nav bar for main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React, {useState} from "react";
-import './App.css';
-import { Login } from './components/Login'; 
-import { Register } from './components/Register'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navigation/Navbar.js";
 
-//Login and Register Function
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
-    <div className="App">
-      {
-        //ternary operator to check if condition is true and whats displayed
-        currentForm === "login" ?   <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
-      }
- 
+    <div>
+      <Navbar />
     </div>
   );
 }
 
+
 export default App;
+// import React, {useState} from "react";
+// import './App.css';
+// import { Login } from './components/login'; 
+// import { Register } from './components/register'; 
+// import { Main } from './components/MainBar/mainbar';
+
+//Login and Register Function
+// function App() {
+//   const [currentForm, setCurrentForm] = useState('login');
+
+//   const toggleForm = (formName) => {
+//     setCurrentForm(formName);
+//   }
+
+  // return (
+  //   <div>
+  //     <Main></Main>
+  //   </div>
+  // )
+
+//   return (
+    
+//     <div className="App">
+//       {
+//         //ternary operator to check if condition is true and whats displayed
+//         currentForm === "login" ?   <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+//       }
+ 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React, { Component } from "react";
+
+// import { MenuItems } from './MenuItems';
+// import './Navbar.css';
+// // import { Button } from '../Button';
+
+// class Navbar extends Component {
+//   state = { clicked: false }
+
+//   handleClick = () =>{
+//     this.setState({ clicked: !this.state.clicked })
+//   }
+
+//   render() {
+//     return(
+//       <nav className="NavbarItems">
+//         <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+//         <div className="menu-icon" onClick={this.handleClick}>
+//           <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+//         </div>
+//         <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+//           {MenuItems.map((item, index) => {
+//             return (
+//               <li key={index}>
+//                 <a className={item.cName} href={item.url}>
+//                   {item.title}
+//                 </a>
+//               </li>
+//             )
+//           })}          
+//         </ul>
+//         {/* <Button></Button> */}
+//       </nav>
+//     )
+//   }
+// }
+
+
+
+
+// export default Navbar;
---
+++ src/components/MainBar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'About',
+        url: 'href="./About/index.js',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: 'href="#contact',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Not sure yet',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
+
+// export default MenuItems;
